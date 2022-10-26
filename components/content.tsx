26ca84865923520075079a3ef417d35ad0703f24
Milad Nekofar <milad@nekofar.com>
2022-10-26 17:10:31 +0330

    feat: improve index by adding base structure and components

---
+++ components/content.tsx
@@ -0,0 +1,13 @@
+import {Hero} from "./content/hero";
+
+export default function Content() {
+  return (
+    <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 min-h-screen">
+      <Hero/>
+
+      <section>
+
+      </section>
+    </main>
+  )
+}
