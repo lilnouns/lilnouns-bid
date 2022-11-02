0d241f60ef837b01f981081ea844c30b41b6ae89
Milad Nekofar <milad@nekofar.com>
2022-11-02 12:08:27 +0330

    refactor: add `useLilNoun` hook to get lilnoun by token id

---
+++ hooks/useLilNoun.ts
@@ -0,0 +1,22 @@
+import {useContractRead} from "wagmi";
+import LilNounsTokenReference from "../json/lilnouns-token.json";
+
+export type LilNoun = {
+  name: string
+  image: string
+  description: string
+}
+
+export function useLilNoun(tokenId: number) {
+  const {data: dataURI, error} = useContractRead({
+    address: LilNounsTokenReference.address,
+    abi: LilNounsTokenReference.abi,
+    functionName: 'dataURI',
+    args: [tokenId]
+  });
+
+  const json = Buffer.from((dataURI as string || "").substring(29), 'base64').toString();
+  const data = JSON.parse(json || "{}") as LilNoun;
+
+  return {data, error}
+}
