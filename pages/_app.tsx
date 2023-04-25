f36baf6d8613c388a4ed1ce067ecf982ef758edb
Milad Nekofar <milad@nekofar.com>
2023-04-25 15:37:23 +0330

    refactor: replace `UrqlProvider` by `withUrqlClient`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,15 @@ import SEO from '../next-seo.config';
 import {configureChains, createClient, WagmiConfig} from "wagmi";
 import {infuraProvider} from 'wagmi/providers/infura'
 import {getDefaultWallets, RainbowKitProvider} from "@rainbow-me/rainbowkit";
-import {createClient as urqlCreatClient, Provider as UrqlProvider} from 'urql';
+import {cacheExchange, fetchExchange} from 'urql';
 import {goerli, mainnet} from 'wagmi/chains'
+import {withUrqlClient} from 'next-urql'
 
 const defaultChains = [mainnet, goerli]
 
-const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY!;
+const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY as string;
 
-const graphQlApiUrl = process.env.NEXT_PUBLIC_GRAPHQL_API_URL!;
+const graphQlApiUrl = process.env.NEXT_PUBLIC_GRAPHQL_API_URL as string;
 
 const {chains, provider, webSocketProvider} = configureChains(defaultChains, [
   infuraProvider({
@@ -33,21 +34,21 @@ const wagmiClient = createClient({
   webSocketProvider
 })
 
-const urqlClient = urqlCreatClient({
-  url: graphQlApiUrl,
-});
-
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains} modalSize="compact">
-        <UrqlProvider value={urqlClient}>
-          <DefaultSeo {...SEO} />
-          <Component {...pageProps} />
-        </UrqlProvider>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
   );
 }
 
-export default MyApp
+export default withUrqlClient(
+  (ssrExchange) => ({
+    url: graphQlApiUrl,
+    exchanges: [cacheExchange, ssrExchange, fetchExchange],
+  }),
+  {ssr: false}
+)(MyApp)
